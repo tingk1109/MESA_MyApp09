e1367b5c597ae60cf5b4a3eb543d2ffaed48b18f
user <user@userde-MacBook-2.local>
2017-06-27 16:26:47 +0800

    v1

--- MyApp09/ViewController.swift
+++ MyApp09/ViewController.swift
@@ -10,16 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
+    private var a = 0
+    
+    @IBAction func test1(_ sender: Any) {
+        DispatchQueue.global().async {
+            self.task1()
+        }
+        
+    }
+    
+    @IBAction func test2(_ sender: Any) {
+        DispatchQueue.global().async {
+            self.task2()
+        }
+    
+    }
+    
+    func task1(){
+        for i in 1...10{
+            print("A: \(i)")
+            //sleep(1)
+            usleep(1000 * 1000)
+            DispatchQueue.main.async {
+                self.label1.text = String(i)
+                
+            }
+        }
+    }
+    
+    func task2(){
+        for i in 1...10{
+            print("B: \(i)")
+            usleep(1000 * 1000)
+            DispatchQueue.main.async {
+                self.label2.text = String(i)
+                
+            }
+        }
+    }
+    
+    
+    @IBAction func test3(_ sender: Any) {
+        let opq = OperationQueue()
+        opq.addOperation {
+            for i in 1...100{
+                sleep(1)
+                DispatchQueue.main.async {
+                    self.label1.text = String(i)
+                }
+            }
+        }
+        opq.addOperation {
+            for i in 1...100{
+                sleep(1)
+                DispatchQueue.main.async {
+                    self.label2.text = String(i)
+                }
+            }
+        }
+    }
+    
+    
+    @IBAction func test4(_ sender: Any) {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(Timer) in self.task4()
+        })
+    }
+    
+    func task4(){
+        a += 1
+        label1.text = String(a)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.label1.text = String(100)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
